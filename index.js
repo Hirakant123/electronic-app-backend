c12efe54a775e1e1acc97835701a33f2fbdcba6d
Hirakant Mukade <168952433+Hirakant123@users.noreply.github.com>
2025-03-06 15:00:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const { todoRouter } = require("./routes/todo.routes");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(
