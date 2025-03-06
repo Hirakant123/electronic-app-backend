65eaf3699ec14fdff49a59ebe1dd55aac2f410cc
Hirakant Mukade <168952433+Hirakant123@users.noreply.github.com>
2025-03-06 14:54:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // This points to your existing db.json file
@@ -11,3 +11,4 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`JSON Server is running on port ${PORT}`);
 });
+*/
